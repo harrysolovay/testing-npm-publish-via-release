a90305cee9c45b884a2354ec230ea8423caf0cd7
Harry Solovay <harrysolovay@gmail.com>
2024-03-29 14:11:05 -0400

    first commit

---
+++ index.ts
@@ -0,0 +1,9 @@
+/**
+ * add two numbers together
+ * @param a the first number
+ * @param b the second number
+ * @returns the sum of the first and second numbers
+ */
+export function add(a: number, b: number): number {
+  return a + b
+}
